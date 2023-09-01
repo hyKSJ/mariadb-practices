8999528972314dc2a76722af9259f57fae26d873
hyKSJ <tkdwns9494@gmail.com>
2023-09-01 11:15:14 +0900

    transation

--- sql연습/ddl-dml연습.sql
+++ sql연습/ddl-dml연습.sql
@@ -38,5 +38,45 @@ desc member;
 -- insert
 insert 
 	into member (no, email, password, name, dept, self_intro)
-    values (null, 'tkdwns9494@gmail.com', password('1234'), '김상준', '개발팀', null)
+    values (null, 'tkdwns9494@gmail.com', password('1234'), '김상준', '개발팀', null);
     select * from member;
+    
+insert 
+	into member (email, password, name, dept)
+    values ('tkdwns9494@gmail.com', password('1234'), '김상준', '개발팀');
+    select * from member;
+
+insert 
+	into member (email, name, dept, password)
+    values ('tkdwns9494@gmail.com', '김상준', '개발팀', password('1234'));
+    select * from member;
+    
+-- update
+update member
+	set email='ksj@gmail.com', name='준'
+    where no = 4;
+    
+select * from member;
+
+-- delete
+delete 
+	from member 
+	where no = 4;
+    
+select * from member;
+
+
+-- transaction begin
+set autocommit = 0;
+select @@autocommit from dual;
+
+insert 
+	into member (email, password, name, dept)
+    values ('tkdwns9494@gmail.com', password('1234'), '김상준', '개발팀');
+    select * from member;
+
+select no,email,dept  from member;
+
+commit;
+
+select no,email,dept  from member;
